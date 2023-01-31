b95ab45e62e47d7179723444c470e6013c79db48
Lucca Faria Ferri <habboclub97@gmail.com>
2023-01-31 07:34:32 -0800

    Update.

--- src/DiIiS-NA/REST/Config.cs
+++ src/DiIiS-NA/REST/Config.cs
@@ -10,7 +10,7 @@ namespace DiIiS_NA.REST
         public string IP { get { return this.GetString("IP", "127.0.0.1"); } set { this.Set("IP", value); } }
         public bool Public { get { return this.GetBoolean("Public", false); } set { this.Set("Public", value); } }
         public string PublicIP { get { return this.GetString("PublicIP", "0.0.0.0"); } set { this.Set("PublicIP", value); } }
-        public int PORT { get { return this.GetInt("PORT", 8081); } set { this.Set("PORT", value); } } //8081
+        public int Port { get { return this.GetInt("PORT", 8081); } set { this.Set("PORT", value); } } //8081
         private static readonly Config _instance = new Config();
         public static Config Instance { get { return _instance; } }
         private Config() : base("REST") { }
